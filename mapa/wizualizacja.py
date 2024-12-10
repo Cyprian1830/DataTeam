f9797dd5d9d950cea5fef5626a0dd807e96d3c47
zurekbartek <zurekbartek1@gmail.com>
2024-12-10 15:54:33 +0100

    wszystko hihi

--- mapa/wizualizacja.py
+++ mapa/wizualizacja.py
@@ -3,9 +3,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
-gminy_shp = gpd.read_file("C:\\Users\\zurek\\OneDrive\\Pulpit\\DataTeam-main\\DataTeam\\mapa\\gminy.shp")
+gminy_shp = gpd.read_file("C:\\Users\\zurek\\OneDrive\\Pulpit\\gminy z przyrostem\\powiaty\\A02_Granice_powiatow.shp")
 
-data = pd.read_excel("C:\\Users\\zurek\\OneDrive\\Pulpit\\DataTeam-main\\DataTeam\\mapa\\Tabela_IV.xls")
+data = pd.read_excel("C:\\Users\\zurek\\OneDrive\\Pulpit\\gminy z przyrostem\\pliki xslx\\Tabela_III.xls")
 data = data.iloc[9:, [1, 18]]
 data.columns = ['JPT_KOD_JE', 'Przyrost naturalny']  
 
@@ -16,10 +16,9 @@ data['Przyrost naturalny'] = pd.to_numeric(data['Przyrost naturalny'], errors='c
 
 gminy_joined = gminy_shp.merge(data, on='JPT_KOD_JE', how='left')
 
-
 bins = [-np.inf, -20, -15, -13, -10, -8, -5, -3, -1, 0, 1, 2, 3, 4, 5, 7, 8, 9, 11, 12, np.inf]
-labels = ['<-20', '-20 do -15', '-15 do -13', '-13 do -10', '-10 do -8', '-8 to -5', '-5 do -3', '-3 do -1', '-1 do 0',
-          '0 do 1', '1 do 2', '2 do 3', '3 do 4', '4 do 5', '5 do 7', '7 do 8', '8 do 9', '9 do 11', '11 do 12', '>11']
+labels = ['<-20', '-20 do -15', '-15 to -13', '-13 do -10', '-10 do -8', '-8 to -5', '-5 do -3', '-3 to -1', '-1 to 0',
+          '0 do 1', '1 do 2', '2 do 3', '3 do 4', '4 do 5', '5 do 7', '7 do 8', '8 to 9', '9 to 11', '11 to 12', '>11']
 gminy_joined['Przyrost naturalny przedział'] = pd.cut(gminy_joined['Przyrost naturalny'], bins=bins, labels=labels)
 
 fig, ax = plt.subplots(figsize=(12, 12))
@@ -30,3 +29,4 @@ gminy_joined.plot(column='Przyrost naturalny przedział', ax=ax, legend=True, cm
 
 plt.title("Mapa przyrostu naturalnego w gminach, rok 2023")
 plt.show()
+
